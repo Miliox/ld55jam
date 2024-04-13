ee8d7a5ea4f0f38bad246458f969165ffb53baef
Jonathan Karlsson <karlsjona@gmail.com>
2024-04-13 15:12:21 +0200

    spell drawing

--- src/spells.lua
+++ src/spells.lua
@@ -1,2 +1,27 @@
+sprites = {
+    summon1 = 10,
+    summon2 = 11,
+}
 
-spells = {}
+spells = {
+    {
+        sprite = sprites.summon1,
+        func = function(caster)
+            add_monster(caster.pos)
+        end
+    },
+    {
+        sprite = sprites.summon2,
+        func = function(caster)
+            add_monster(caster.pos)
+        end
+    }
+}
+
+function draw_spells()
+    local x = 1
+    for spell in all(spells) do
+        spr(spell.sprite, x, 128 - 9)
+        x = x + 9
+    end
+end
